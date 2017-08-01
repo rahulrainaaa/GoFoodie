567f803dd3f79e8a11ac12c84bf61ac757331ae2
rahulrainahomeee <rahulrainahome@gmail.com>
2017-08-01 15:20:04 +0530

    Initial Commit

--- app/src/main/java/com/app/gofoodie/activity/base/BaseAppCompatActivity.java
+++ app/src/main/java/com/app/gofoodie/activity/base/BaseAppCompatActivity.java
@@ -1,10 +1,26 @@
 package com.app.gofoodie.activity.base;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * @class BaseAppCompatActivity
  * @desc {@link AppCompatActivity} Base class for application customization.
  */
 public class BaseAppCompatActivity extends AppCompatActivity {
+
+    /**
+     * @method showFullScreen
+     * @desc Method to show full screen by hiding title, navigation and status bar.
+     */
+    protected void showFullScreen() {
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+    
 }
--- app/src/main/java/com/app/gofoodie/activity/derived/SplashActivity.java
+++ app/src/main/java/com/app/gofoodie/activity/derived/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.app.gofoodie.activity.derived;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
 
 import com.app.gofoodie.R;
 import com.app.gofoodie.activity.base.BaseAppCompatActivity;
@@ -9,11 +11,43 @@ import com.app.gofoodie.activity.base.BaseAppCompatActivity;
  * @class SplashActivity
  * @desc Activity Class to show Splash screen on the application start.
  */
-public class SplashActivity extends BaseAppCompatActivity {
+public class SplashActivity extends BaseAppCompatActivity implements Runnable {
 
+    /**
+     * Class private data objects.
+     */
+    private Handler mHandler = null;
+
+    /**
+     * {@link BaseAppCompatActivity} callback method.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        showFullScreen();
+        mHandler = new Handler();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mHandler.postDelayed(this, 500);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mHandler.removeCallbacks(this);
+    }
+
+    /**
+     * {@link Runnable} interface callback method.
+     */
+    @Override
+    public void run() {
+        Toast.makeText(this, "Runnable splash toast...", Toast.LENGTH_SHORT).show();
+//        startActivity(new Intent(this, DashboardActivity.class);
+//        finish();
     }
 }
