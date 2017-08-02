0f440a9aecafc22d625d7e5990cbff30564ef0cc
rahulrainahomeee <rahulrainahome@gmail.com>
2017-08-02 15:58:16 +0530

    Initial Commit

---
+++ app/src/main/java/com/app/gofoodie/fragment/derived/NetworkErrorFragment.java
@@ -0,0 +1,34 @@
+package com.app.gofoodie.fragment.derived;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.app.gofoodie.R;
+import com.app.gofoodie.fragment.base.BaseFragment;
+
+/**
+ * @class NetworkErrorFragment
+ * @desc {@link BaseFragment} Fragment class to handle Network-Error UI screen.
+ */
+public class NetworkErrorFragment extends BaseFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.activity_splash, container, false);
+        Toast.makeText(getActivity(), "fragment-onCreateView()-method called.", Toast.LENGTH_SHORT).show();
+        //super.onCreateView(inflater, container, savedInstanceState);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Toast.makeText(getActivity(), "fragment view destroyed.", Toast.LENGTH_SHORT).show();
+    }
+}
