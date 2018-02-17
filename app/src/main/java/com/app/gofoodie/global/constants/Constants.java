0a2507cc7b62af3c51faa9267694992084ed7f7d
rahulrainahomeee <rahulrainahome@gmail.com>
2018-02-17 17:47:17 +0530

    Initial Commit.

--- app/src/main/java/com/app/gofoodie/global/constants/Constants.java
+++ app/src/main/java/com/app/gofoodie/global/constants/Constants.java
@@ -29,7 +29,7 @@ public class Constants {
     public static final String ADMIN_PHONE_NUMBER = "0987654321";
     public static final String ADMIN_EMAIL = "info@gofoodie.com";
 
-    public static final String REGEX_EMAIL = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    public static final String REGEX_EMAIL = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]+)?$";
     public static final String REGEX_MOBILE = "^[0-9]{10,15}$";
     public static final String REGEX_NAME = "^[\\p{L} .'-]+$";
 
